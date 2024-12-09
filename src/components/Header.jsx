d36e89755547c18318e2a0da698364574e08a3c5
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-12-09 19:49:11 +0530

    minor adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,25 @@ const Header = () => {
     return (
         <div className=" px-5 lg:px-[50px] w-full shadow-sm p-4 bg-[#131313] text-white border-b border-gray-800">
             <div className="flex justify-between">
-                <h2 className="font-extrabold text-[20px] lg:text-[25px] text-center ">
-                    Fusion <span className="text-[#4b02e8]">News</span>
-                </h2>
-                <div className="flex gap-5 ">
+                <div className="flex gap-2 justify-center">
+                    <img
+                        className="h-5 w-5  lg:h-7 lg:w-7 flex "
+                        src="/favicon.png"
+                        alt=""
+                    />
+                    <div className="font-extrabold text-[15px] lg:text-[20px] ">
+                        Fusion <span className="text-[#4b02e8]">News</span>
+                    </div>
+                </div>
+
+                <div className="flex gap-3 lg:gap-5 ">
                     <a
                         href="https://github.com/armanulhaq"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         <img
-                            className="w-5 h-5"
+                            className="w-4 h-4 lg:w-5 lg:h-5"
                             src="/github.png"
                             alt="GitHub"
                         />
@@ -23,7 +31,7 @@ const Header = () => {
                         rel="noopener"
                     >
                         <img
-                            className="w-5 h-5"
+                            className="w-4 h-4 lg:w-5 lg:h-5"
                             src="/linkedin.png"
                             alt="Linkedin"
                         />
@@ -33,7 +41,11 @@ const Header = () => {
                         target="_blank"
                         rel="noopener"
                     >
-                        <img className="w-5 h-5" src="/portfolio.png" alt="" />
+                        <img
+                            className="w-4 h-4 lg:w-5 lg:h-5"
+                            src="/portfolio.png"
+                            alt=""
+                        />
                     </a>
                 </div>
             </div>
